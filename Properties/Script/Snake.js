b8c67d07eba350db2c62465e6332e33bc598a5a0
MacCloud <cameronmaccloud13@gmail.com>
2025-09-13 23:43:37 +0800

    Index.html ui is almost finished, added /0/ directory for pages

--- Properties/Script/Snake.js
+++ Properties/Script/Snake.js
@@ -3,3 +3,26 @@ const border_back = document.querySelector('.border_back');
 setInterval(() => {
     border_back.classList.toggle('spin');
 }, 10000);
+
+function viewMain(){
+    const article = document.getElementById('article');
+    const section = document.getElementById('section');
+    const idle = document.getElementById('idle');
+    const background = document.getElementById('background');
+    article.classList.add('out');
+    section.classList.add('out');
+    setTimeout(() => {
+        background.style.cursor = 'wait';
+        if (idle) {
+            idle.classList.add('active');
+        } else {
+            console.warn("Element with id 'idle' not found.");
+        }
+        section.style.display = 'none';
+        article.style.display = 'none';
+        console.log('Go to main page');
+    }, 400);
+    setTimeout(() => {
+        window.location.href = "0/main";
+    }, 1000);
+}
